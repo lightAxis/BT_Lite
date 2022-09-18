3f968f3e996e471f56be0d96e4dfd425f26d6415
jisuk500 <jisuk500@gmail.com>
2022-09-19 00:40:20 +0900

    add groot xml parser py

---
+++ generator/BT_generator.py
@@ -0,0 +1,36 @@
+#!usr/bin/env python3
+
+from dataclasses import dataclass
+from typing import List, Dict
+import os
+import sys
+import argparse
+
+try:
+    import em
+except ImportError as e:
+    print("Failed to import em in python3 :", str(e))
+    print("")
+    print("you may need to install it using")
+    print(" pip3 install --user empy")
+    print("")
+    sys.exit(1)
+
+from GrootXMLParser.GrootXMLParser import GrootXMLParser as GXMLP
+from Generator.Generator import Generator as CodeGen
+
+
+if __name__ == "__main__":
+    print("this is main")
+    now = os.path.abspath(__file__)
+    now_dir = os.path.dirname(now)
+    xml_path = os.path.join(now_dir, "test.xml")
+    print(now)
+    print(now_dir)
+    print(xml_path)
+
+    Parser: GXMLP = GXMLP(xml_path)
+    ParserResult = Parser.Parse()
+
+else:
+    raise NameError("Strange started without main")
---
+++ generator/GrootXMLParser/ParseTotalTree.py
@@ -0,0 +1,133 @@
+#!usr/bin/env python3
+
+from .EnumsStructs import *
+
+
+def Parse(MainSubTree: ETree.Element, SubTrees: List[SubTree_t], CustomNodes: List[TreeNodesModel_t]) -> Tuple[List[TotalTree_t], List[ParentChild_t]]:
+    resultsTotalTree: List[TotalTree_t] = []
+    resultsParentChild: List[TotalTree_t] = []
+
+    _uid: int = 1
+    root: TotalTree_t = __parseTatalTree_t_Root(MainSubTree)
+    root.UID = _uid
+
+    resultsTotalTree.append(root)
+
+    for info in resultsTotalTree:
+        children: ETree.Element = __expandChildrenFromTreeNode(
+            node=info.TreeNodePtr, subTrees=SubTrees)
+
+        if len(children) > 0:
+            new_ParentChild: ParentChild_t = ParentChild_t()
+            new_ParentChild.ParentIdx = _uid - 1
+            new_ParentChild.ChildrenIdxs = list(
+                range(len(resultsTotalTree), len(resultsTotalTree)+len(children)))
+            resultsParentChild.append(new_ParentChild)
+
+        for child in children:
+            new_TotalTree_t: TotalTree_t = __parseTotalTree_t(
+                Node=child, CustomNodes=CustomNodes)
+            _uid = _uid + 1
+            new_TotalTree_t.UID = _uid
+            resultsTotalTree.append(new_TotalTree_t)
+
+    return resultsTotalTree, resultsParentChild
+    pass
+
+
+def __expandChildrenFromTreeNode(node: ETree.Element, subTrees: List[SubTree_t]) -> ETree.Element:
+    if node.tag == 'SubTree':
+        for subt in subTrees:
+            if subt.Name == node.attrib["ID"]:
+                return subt.NodePtr
+        raise NameError("No matching subtree name in Subtree lists")
+    else:
+        return node
+    pass
+
+
+def __parseTatalTree_t_Root(Node: ETree.Element) -> TotalTree_t:
+    result: TotalTree_t = TotalTree_t()
+    result.Attrib = Node.attrib
+    result.ChildNum = 1
+    result.NodeType = eNodeType.SubTree
+    result.Tag = "BehaviorTree"
+    result.isCustom = False
+    result.Name = "RootTree"
+    result.TreeNodePtr = Node
+    if Node.attrib['ID'] != "RootTree":
+        raise NameError("RootTree name is strange, is if named 'RootTree' ??")
+    return result
+    pass
+
+
+def __parseTotalTree_t(Node: ETree.Element, CustomNodes: List[TreeNodesModel_t]) -> TotalTree_t:
+    result: TotalTree_t = TotalTree_t()
+    # parse Tag
+    result.Tag = Node.tag
+    # parse Attrib
+    result.Attrib = Node.attrib
+    # parse TreeNodePtr
+    result.TreeNodePtr = Node
+    # parse ChildNum
+    result.ChildNum = len(Node)
+    # parse NodeType, Name, isCustom, CustomPtr
+    result.NodeType, result.Name, result.isCustom, result.CustomPtr = __parseNodeTypeFromNode(
+        Tag=Node.tag, ID=Node.attrib.get('ID'), CustomNodes=CustomNodes)
+
+    return result
+    pass
+
+
+def __parseNodeTypeFromNode(Tag: str, ID: str, CustomNodes: List[TreeNodesModel_t]) -> Tuple[eNodeType, str, bool, TreeNodesModel_t]:
+    nodeType: eNodeType = None
+    nodeName: str = None
+    isCustomType: bool = False
+    CustomPtr: TreeNodesModel_t = None
+
+# check is base type
+    if ID == None:
+        for enum in eAction_base:
+            if Tag == enum.name:
+                nodeType = eNodeType.Action
+                nodeName = enum.name
+                break
+        if (nodeType == None):
+            for enum in eCondition_base:
+                if Tag == enum.name:
+                    nodeType = eNodeType.Condition
+                    nodeName = enum.name
+                    break
+        if (nodeType == None):
+            for enum in eControl_base:
+                if Tag == enum.name:
+                    nodeType = eNodeType.Control
+                    nodeName = enum.name
+                    break
+        if (nodeType == None):
+            for enum in eDecorator_base:
+                if Tag == enum.name:
+                    nodeType = eNodeType.Decorator
+                    nodeName = enum.name
+                    break
+        if (nodeType == None):
+            for enum in eSubTree_base:
+                if Tag == enum.name:
+                    nodeType = eNodeType.SubTree
+                    nodeName = enum.name
+                    break
+        if (nodeType != None):
+            isCustomType = False
+            return nodeType, nodeName, isCustomType, None
+    else:
+        # check if custom type
+        for node in CustomNodes:
+            if ID == node.ID:
+                nodeType = node.NodeType
+                isCustomType = True
+                CustomPtr = node
+                nodeName = node.ID
+                return nodeType, nodeName, isCustomType, CustomPtr
+
+    raise NameError("Cannot parse node type from base & custom node infos")
+    pass
